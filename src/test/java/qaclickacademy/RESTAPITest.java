fa863bf28004f9a282349dfdfc475586cfe8b72c
andre <andreasvysatria@gmail.com>
2019-04-08 16:36:06 +0800

    commit from yolo

--- src/test/java/qaclickacademy/RESTAPITest.java
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -11,6 +11,9 @@ public class RESTAPITest {
 		System.out.println("postJira2");
 		System.out.println("postJira3");
 		System.out.println("postJira4");
+		System.out.println("postJira5");
+		System.out.println("postJira6");
+		System.out.println("postJira7");
 	}
 	
 	@Test
